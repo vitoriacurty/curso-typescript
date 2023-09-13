f912af4c55ab463a98b123c4225fa3d226a44934
vitoriacurty <vitoria.curty@hotmail.com>
2023-09-12 22:25:33 -0300

    resolução dos exercícios 1, 2 e 3

---
+++ aula-ts-01/index.ts
@@ -0,0 +1,6 @@
+function somar(num1: number, num2: number) {
+    return num1 + num2;    
+}
+
+const resultado = somar("string", 3);
+console.log(resultado);
--- src/aula-ts-01/index.ts
+++
@@ -1,6 +0,0 @@
-function somar(num1: number, num2: number) {
-    return num1 + num2;
-}
-
-const resultado = somar(1, 3);
-console.log(resultado);
