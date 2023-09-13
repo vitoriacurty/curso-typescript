72282fc4882b9c036eb8d0a344bfd1ba8d107fe3
vitoriacurty <vitoria.curty@hotmail.com>
2023-09-12 23:07:35 -0300

    resolução dos exercícios 4 e 5

---
+++ aula-ts-04/index.ts
@@ -0,0 +1,30 @@
+function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+function isEven(num: number): boolean {
+    return num % 2 === 0;
+}
+
+function showResult(result: number): void {
+    if(isEven(result)) {
+        console.log(`The result is ${result} and it's even!`);
+    } else {
+        console.log(`The result is ${result} and it's even!`);
+    }
+}
+
+(() => {
+    const num1 = prompt("First Number");
+    const num2 = prompt("Second Number");
+    
+    let result = sum(num1,num2);
+    result += multiply(1,2);
+    showResult(result);
+})();
+
+// fix later
